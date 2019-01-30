f849d69d3802f50f11986439af72646585ac2bf6
Anatolii Pisarivskyi <pisarivskii@gmail.com>
2019-01-30 18:14:21 +0200

    update Data component

--- src/containers/data/Data.jsx
+++ src/containers/data/Data.jsx
@@ -1,77 +1,85 @@
 import { h, Component } from 'preact';
 import { Link } from 'preact-router';
+import { Gender } from '../../components/gender/Gender';
+import { Height } from '../../components/height/Height';
 
 const nextArrowIcon = require('../../images/arrow.svg');
 
 export class Data extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      gender: null,
+      height: null,
+      validation: {
+        gender: true,
+        height: true,
+        agree: true,
+      },
+    };
+  }
+
+  changeGender = (gender) => {
+    this.setState({
+      gender,
+      validation: {
+        height: this.state.validation.height,
+        gender: (gender === 'male' || gender === 'female'),
+      }
+    });
+  }
+
+  changeHeight = (height) => {
+    let isValueValid = false;
+    const numHeight = parseInt(height);
+
+    if (numHeight >= 150 && numHeight <= 220) {
+      isValueValid = true;
+    }
+
+    this.setState({
+      height: numHeight,
+      validation: {
+        height: isValueValid,
+        gender: this.state.validation.gender,
+        agree: false,
+      }
+    });
+  }
+
+  onNextScreen = () => {
+    console.log(this.state);
   }
 
   render() {
     return (
       <div class="screen screen--data active">
         <div class="screen__content data">
-          <h2 class="screen__title">And the last step</h2>
+          <h2 class="screen__title">PLEASE ENTER YOUR DATA</h2>
           <p class="screen__text">Please select your gender and enter height. <br />
           We need this information to create your Perfect Fit Profile</p>
 
           <div class="data__block">
             <div class="data__field">
               <h3 class="data__field-title">Gender:</h3>
-              <div class="gender">
-                <input type="radio" name="gender" id="gender-female" value="female" />
-                <label class="gender__item" for="gender-female">Female</label>
-
-                <input type="radio" name="gender" id="gender-male" value="male" />
-                <label class="gender__item" for="gender-male">Male</label>
-              </div>
+              
+              <Gender change={this.changeGender} isValid={this.state.validation.gender} />
             </div>
 
             <div class="data__field">
               <h3 class="data__field-title">Height:</h3>
 
-              <div class="height" data-measure="in">
-
-                <div class="height__measure height__measure--cm">
-                  <div class="height__input-block" data-measure="cm">
-                    <input class="height__input" type="text" />
-                  </div>
-                </div>
-
-                <div class="height__measure height__measure--in">
-                  <div class="height__input-block" data-measure="ft">
-                    <input class="height__input" type="text" />
-                  </div>
-                  <div class="height__input-block" data-measure="in">
-                    <input class="height__input" type="text" />
-                  </div>
-                </div>
-
-                <div class="height__switcher">
-                  <input type="radio" name="measure" id="measure-cm" value="cm" checked />
-                  <label class="height__switcher-item height__switcher-item--cm" for="measure-cm">cm</label>
-                  
-                  <input type="radio" name="measure" id="measure-in" value="in" />
-                  <label class="height__switcher-item height__switcher-item--in" for="measure-in">in</label>
-
-                  <div class="height__switcher-switch"></div>
-                </div>
-
-                <p class="height__desc">
-                  Your height should be between <br />
-                  150-210 cm
-                </p>
-              </div>
+              <Height change={this.changeHeight} isValid={this.state.validation.height} />
             </div>
           </div>
 
-          <Link class="button" href="/upload">
+          <button class="button" onClick={this.onNextScreen}>
             Next step
             <img class="button__icon" src={nextArrowIcon} alt="Go next arrow icon" />
-          </Link>
+          </button>
 
-          <div class="data__check checkbox">
+          <div class={`data__check checkbox ${!this.state.validation.agree ? 'checkbox--invalid' : ''}`}>
             <input type="checkbox" name="agree" id="agree" />
             <label for="agree">I accept <a href="#">Terms and Conditions</a></label>
           </div>
