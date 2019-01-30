6b015e84a1f046f24bb6b3735018457aa0b84688
Anatolii Pisarivskyi <pisarivskii@gmail.com>
2019-01-30 10:55:47 +0200

    update header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,39 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 
 const saiaPfLogo = require('../../images/logo.svg');
 const modalCloseIcon = require('../../images/close-icon.svg');
 
-export const Header = () => (
-  <div class="header">
-    <button class="header__help">Help</button>
+/**
+ * Widget header component
+ */
+export class Header extends Component {
+  /**
+   * Close button click
+   */
+  onCloseButtonClick = () => {
+    console.log('click');
+  }
 
-    <div class="header__logo">
-      <img src={saiaPfLogo} alt="SAIA Perfect Fit Logo" />
-    </div>
+  /**
+   * Help button click
+   */
+  onHelpButtonClick = () => {
+    console.log('click');
+  }
 
-    <button class="header__close">
-      <img src={modalCloseIcon} alt="Close button icon" />
-    </button>
-  </div>
-);
+  render() {
+    return (
+      <div class="header">
+        <button class="header__help" onClick={this.onHelpButtonClick}>Help</button>
+
+        <div class="header__logo">
+          <img src={saiaPfLogo} alt="SAIA Perfect Fit Logo" />
+        </div>
+
+        <button class="header__close" onClick={this.onCloseButtonClick}>
+          <img src={modalCloseIcon} alt="Close button icon" />
+        </button>
+      </div>
+    );
+  }
+};
