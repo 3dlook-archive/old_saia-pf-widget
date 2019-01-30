fe7ea7cd9c81f05855439c9abe93279fd61c8994
Anatolii Pisarivskyi <pisarivskii@gmail.com>
2019-01-30 13:03:30 +0200

    update Upload comtainer component

--- src/containers/upload/Upload.jsx
+++ src/containers/upload/Upload.jsx
@@ -9,6 +9,34 @@ const nextArrowIcon = require('../../images/arrow.svg');
 export class Upload extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      frontImage: null,
+      sideImage: null,
+      validation: {
+        pose: false,
+        body: false,
+      },
+      valid: false,
+    };
+  }
+
+  /**
+   * Save front image to state
+   */
+  saveFrontFile = (params) => {
+    this.setState({
+      frontImage: params.fileBlob,
+    });
+  }
+
+  /**
+   * Save side image to state
+   */
+  saveSideFile = (params) => {
+    this.setState({
+      sideImage: params.fileBlob,
+    });
   }
 
   render() {
@@ -19,11 +47,11 @@ export class Upload extends Component {
           <p class="screen__text">Please upload two full body <br />photos of yourself:</p>
 
           <div class="upload__files">
-            <UploadBlock type="front" />
-            <UploadBlock type="side" />
+            <UploadBlock type="front" validation={this.state.validation} change={this.saveFrontFile} />
+            <UploadBlock type="side" validation={this.state.validation} change={this.saveSideFile} />
           </div>
 
-          <Link class="button" href="/data" disabled>
+          <Link class="button" href="/data" disabled={!this.state.frontImage || !this.state.sideImage}>
             Next step
             <img class="button__icon" src={nextArrowIcon} alt="Go next arrow icon" />
           </Link>
