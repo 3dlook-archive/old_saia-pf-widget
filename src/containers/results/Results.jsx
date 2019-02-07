cd380c62fd13ff523d1b2d8812b00f3d12a91334
Anatolii Pisarivskyi <pisarivskii@gmail.com>
2019-02-07 14:22:11 +0200

    update Results component

--- src/containers/results/Results.jsx
+++ src/containers/results/Results.jsx
@@ -1,23 +1,23 @@
 import { h } from 'preact';
 import classNames from 'classnames';
+import { send } from '../../utils';
 
 const nextArrowIcon = require('../../images/arrow.svg');
-const productImagePlaceholder = require('../../images/product-img.png');
 
 const Results = ({ matches }) => (
   <div className="screen screen--result active">
     <div className="screen__content result">
       <h2 className="screen__title result__title">your recomended size</h2>
 
-      {/* <div className="result__product">
+      <div className={classNames('result__product', { active: matches.product_description && matches.image })}>
         <div className="result__product-img">
-          <img src={productImagePlaceholder} alt="" />
+          <img src={matches.image} alt="" />
         </div>
         <div className="result__product-info">
           <h3 className="result__product-title">For this product:</h3>
-          <p className="result__product-desc">The Nike Air Rally Women's Crew</p>
+          <p className="result__product-desc">{matches.product_description}</p>
         </div>
-      </div> */}
+      </div>
 
       <div className="result__sizes">
         <div className={classNames('result__size', 'result__size--tight', { active: matches.tight })}>
@@ -44,7 +44,7 @@ const Results = ({ matches }) => (
         Size recommendations for other products are now available.
       </p>
 
-      <button className="button" type="button">
+      <button className="button" type="button" onClick={() => send('close')}>
         back to store
         <img className="button__icon" src={nextArrowIcon} alt="Go next arrow icon" />
       </button>
