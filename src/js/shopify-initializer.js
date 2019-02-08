2e1a614c04cc360f1671da6e8c8f6663941305cd
Anatolii Pisarivskyi <pisarivskii@gmail.com>
2019-02-08 12:38:42 +0200

    update shopify initializer script

--- src/js/shopify-initializer.js
+++ src/js/shopify-initializer.js
@@ -9,19 +9,27 @@ import SaiaButton from './button';
  *   if so, get recomended size from api
  * 4. display button
  */
-const button = new SaiaButton({
-  container: '.product-single__meta',
-  key: API_KEY,
-  widgetUrl: WIDGET_HOST,
-  brand: '123123123',
-  bodyPart: 'top',
-  product: {
-    imageUrl: 'https://gamemag.ru/images/views/no_avatar.jpg',
-    description: 'The Nike Air Rally Women\'s Crew',
-  },
-});
-button.init();
+(async () => {
+  const button = new SaiaButton({
+    container: '.product-single__meta',
+    key: API_KEY,
+    widgetUrl: WIDGET_HOST,
+    brand: '123123123',
+    bodyPart: 'top',
+    product: {
+      imageUrl: 'https://gamemag.ru/images/views/no_avatar.jpg',
+      description: 'The Nike Air Rally Women\'s Crew',
+    },
+  });
+  const isButtonVisible = button.checkButtonVisibility();
 
-console.log(API_KEY);
-console.log(API_HOST);
-console.log(WIDGET_HOST);
+  if (isButtonVisible) {
+    button.init();
+
+    const recomendations = await button.getSize();
+
+    if (recomendations) {
+      button.displaySize(recomendations);
+    }
+  }
+})();
