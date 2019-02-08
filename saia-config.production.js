e5b351c02c274faad6107b6161c8e5c6a89dfb14
Anatolii Pisarivskyi <pisarivskii@gmail.com>
2019-02-08 12:38:05 +0200

    update production config

--- saia-config.production.js
+++ saia-config.production.js
@@ -1,5 +1,10 @@
 module.exports = {
+  // saia api host url
   API_HOST: 'https://saia.3dlook.me',
-  API_KEY: '123',
+  // client api key
+  API_KEY: null,
+  // shopify host url
   SHOPIFY_HOST: 'shopify.3dlook.me',
+  // widget host url
+  WIDGET_HOST: 'https://shopify-widget.3dlook.me',
 };
