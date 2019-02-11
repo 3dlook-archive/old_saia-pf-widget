8997832a4037c86c40901bc1780fa80a6aa64c5e
Anatolii Pisarivskyi <pisarivskii@gmail.com>
2019-02-11 14:06:07 +0200

    update UploadValidation component

--- src/components/upload-validation/UploadValidation.jsx
+++ src/components/upload-validation/UploadValidation.jsx
@@ -1,10 +1,12 @@
 import { h } from 'preact';
 
+import classNames from 'classnames';
+
 const crossIcon = require('../../images/cross.svg');
 const checkIcon = require('../../images/check.svg');
 
 const UploadValidation = ({ validation }) => (
-  <ul className="upload__file-validation">
+  <ul className={classNames('upload__file-validation', { active: validation.pose || validation.body })}>
     <li className={`upload__file-validation-status upload__file-validation-status--${validation.pose}`}>
       <span className="upload__file-validation-icon">
         <img className="valid" src={checkIcon} alt="Check icon" />
