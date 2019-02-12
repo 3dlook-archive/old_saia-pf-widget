37c4a21a6a5d0616cbbc9181698bc35efd78435c
Anatolii Pisarivskyi <pisarivskii@gmail.com>
2019-02-12 11:41:48 +0200

    add utils tests

---
+++ test/utils.test.js
@@ -0,0 +1,63 @@
+import { expect } from 'chai';
+
+import {
+  transformRecomendations,
+  objectToUrlParams,
+  in2cm,
+  ft2in,
+  cmToFtIn,
+} from '../src/utils';
+
+describe('utils', () => {
+  describe('transformRecomendations', () => {
+    it('should transform object correctly', () => {
+      const recomendations = {
+        normal: {
+          size: 'M',
+          accuracy: 0.6933732682744242,
+          body_part: 'chest',
+        },
+      };
+
+      const transformedRecomendations = transformRecomendations(recomendations);
+
+      expect(transformedRecomendations).to.eql({ normal: 'M' });
+    });
+  });
+
+  describe('objectToUrlParams', () => {
+    it('should transform object to query params string', () => {
+      const paramsObject = {
+        param1: 1,
+        param2: '2',
+      };
+
+      const queryString = objectToUrlParams(paramsObject);
+
+      expect(queryString).to.be.equal('param1=1&param2=2');
+    });
+  });
+
+  describe('in2cm', () => {
+    it('should convert inches to centimeters', () => {
+      expect(in2cm(12)).to.equal(30.48);
+      expect(in2cm(20)).to.equal(50.8);
+      expect(in2cm(154)).to.equal(391.16);
+    });
+  });
+
+  describe('ft2in', () => {
+    it('should convert feets to inches', () => {
+      expect(ft2in(1)).to.equal(12);
+      expect(ft2in(3)).to.equal(36);
+      expect(ft2in(15)).to.equal(180);
+    });
+  });
+
+  describe('cmToFtIn', () => {
+    it('should convert centimeters to feets and inches', () => {
+      expect(cmToFtIn(150)).to.eqls({ ft: 4, in: 11 });
+      expect(cmToFtIn(220)).to.eqls({ ft: 7, in: 3 });
+    });
+  });
+});
