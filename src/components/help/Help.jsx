385d9ba320d318cddc4f4eceb122050cd66f6fd8
Anatolii Pisarivskyi <pisarivskii@gmail.com>
2019-03-01 14:07:46 +0200

    add help section content

--- src/components/help/Help.jsx
+++ src/components/help/Help.jsx
@@ -16,20 +16,29 @@ const Help = ({ close, show }) => (
       <h1 className="help__logo">Help</h1>
     </header>
     <div className="help__content">
-      <h2>Question 1</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi beatae quae necessitatibus, alias distinctio dolorum, minus cupiditate consequuntur enim error commodi harum nulla optio ullam obcaecati eveniet eaque asperiores sunt!</p>
-      <h2>Question 1</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi beatae quae necessitatibus, alias distinctio dolorum, minus cupiditate consequuntur enim error commodi harum nulla optio ullam obcaecati eveniet eaque asperiores sunt!</p>
-      <h2>Question 1</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi beatae quae necessitatibus, alias distinctio dolorum, minus cupiditate consequuntur enim error commodi harum nulla optio ullam obcaecati eveniet eaque asperiores sunt!</p>
-      <h2>Question 1</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi beatae quae necessitatibus, alias distinctio dolorum, minus cupiditate consequuntur enim error commodi harum nulla optio ullam obcaecati eveniet eaque asperiores sunt!</p>
-      <h2>Question 1</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi beatae quae necessitatibus, alias distinctio dolorum, minus cupiditate consequuntur enim error commodi harum nulla optio ullam obcaecati eveniet eaque asperiores sunt!</p>
-      <h2>Question 1</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi beatae quae necessitatibus, alias distinctio dolorum, minus cupiditate consequuntur enim error commodi harum nulla optio ullam obcaecati eveniet eaque asperiores sunt!</p>
-      <h2>Question 1</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi beatae quae necessitatibus, alias distinctio dolorum, minus cupiditate consequuntur enim error commodi harum nulla optio ullam obcaecati eveniet eaque asperiores sunt!</p>
+      <h2>SAIA could not detect my body. What am I doing wrong?</h2>
+      <p>Most likely you either didn't fully fit in the frame or took a wrong pose while taking photos. Please check our video guide once again and try to take photos according to our guidelines. Please note that poor lighting might also affect the results. </p>
+
+      <h2>How accurate is SAIA?</h2>
+      <p>Our technology allows us to measure the human body with up to 98% of accuracy. But there might be some issues that decrease accuracy. Thus we advise keeping in mind our recommendations while taking photos.</p>
+
+      <h2>Will the garment that SAIA recommends fit me?</h2>
+      <p>While our body measuring technology is accurate, we cannot guarantee that the garment will 100% fit you, because there's always some difference in garment production or inaccuracy in size charts.</p>
+
+      <h2>How do you calculate my size?</h2>
+      <p>We match your body measurements determined by SAIA with the size chart of the item you've selected.</p>
+
+      <h2>What if my measurements come in between two sizes?</h2>
+      <p>In this case, SAIA will recommend a bigger size.</p>
+
+      <h2>Do I need to upload photos for every item I want to know my size of?</h2>
+      <p>No. We keep your measurements and detect your size for every item, so you don't need to upload your photos. However, we recommend updating your measurements once in a while, especially if your physical form has changed.</p>
+
+      <h2>Do you store my photos? Where do they go after I upload them?</h2>
+      <p>No, we don't store the photos you upload. Photos are uploaded to our server via a secure channel, processed and then deleted. The only information we keep is your measurements - we use them to make size and fit recommendations.</p>
+
+      <h2>I don't see SAIA Perfect Fit button on some store pages.</h2>
+      <p>This means that the seller decided not to use Perfect Fit solution for those items.</p>
     </div>
   </div>
 );
