b3fd5c24af60db91d7ab2014f1d2fb09fe0f44ae
Anatolii Pisarivskyi <pisarivskii@gmail.com>
2019-06-04 11:46:00 +0300

    fix gender component state issue

--- src/components/gender/Gender.jsx
+++ src/components/gender/Gender.jsx
@@ -4,6 +4,10 @@ import { h, Component } from 'preact';
  * Gender component
  */
 export class Gender extends Component {
+  state = {
+    value: null,
+  }
+
   constructor(props) {
     super(props);
   }
@@ -12,17 +16,21 @@ export class Gender extends Component {
    * Gender change event handler
    */
   onGenderChange = (e) => {
-    this.props.change(e.target.value);
+    const { value } = e.target;
+
+    this.props.change(value);
+    this.setState({
+      value,
+    });
   }
 
   render() {
     return (
-
       <div class={`gender ${!this.props.isValid ? 'gender--invalid' : ''}`}>
-        <input type="radio" name="gender" id="gender-female" value="female" onChange={this.onGenderChange} />
+        <input type="radio" name="gender" id="gender-female" value="female" onChange={this.onGenderChange} checked={this.state.value === 'female'} />
         <label class="gender__item" for="gender-female">Female</label>
 
-        <input type="radio" name="gender" id="gender-male" value="male" onChange={this.onGenderChange} />
+        <input type="radio" name="gender" id="gender-male" value="male" onChange={this.onGenderChange} checked={this.state.value === 'male'} />
         <label class="gender__item" for="gender-male">Male</label>
       </div>
     );
