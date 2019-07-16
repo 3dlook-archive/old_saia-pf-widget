46b57c19670a7b782c630b122cf3a7c379eb1a2d
Anatolii Pisarivskyi <pisarivskii@gmail.com>
2019-07-16 11:46:07 +0300

    feat: update gender component

--- src/components/gender/Gender.jsx
+++ src/components/gender/Gender.jsx
@@ -1,15 +1,17 @@
 import { h, Component } from 'preact';
+import className from 'classnames';
+
+const maleIcon = require('../../images/male-icon.png');
+const maleIconX2 = require('../../images/male-icon@2x.png');
+const femaleIcon = require('../../images/female-icon.png');
+const femaleIconX2 = require('../../images/female-icon@2x.png');
 
 /**
  * Gender component
  */
-export class Gender extends Component {
+export default class Gender extends Component {
   state = {
     value: null,
-  }
-
-  constructor(props) {
-    super(props);
   }
 
   /**
@@ -19,6 +21,7 @@ export class Gender extends Component {
     const { value } = e.target;
 
     this.props.change(value);
+
     this.setState({
       value,
     });
@@ -26,12 +29,18 @@ export class Gender extends Component {
 
   render() {
     return (
-      <div class={`gender ${!this.props.isValid ? 'gender--invalid' : ''}`}>
+      <div className={className('gender', this.props.className, { 'gender--invalid': !this.props.isValid })}>
         <input type="radio" name="gender" id="gender-female" value="female" onChange={this.onGenderChange} checked={this.state.value === 'female'} />
-        <label class="gender__item" for="gender-female">Female</label>
+        <label className="gender__item" for="gender-female">
+          <span>Female</span>
+          <img src={femaleIcon} srcSet={`${femaleIcon} 1x, ${femaleIconX2} 2x`} alt="female icon" />
+        </label>
 
         <input type="radio" name="gender" id="gender-male" value="male" onChange={this.onGenderChange} checked={this.state.value === 'male'} />
-        <label class="gender__item" for="gender-male">Male</label>
+        <label className="gender__item" for="gender-male">
+          <span>Male</span>
+          <img src={maleIcon} srcSet={`${maleIcon} 1x, ${maleIconX2} 2x`} alt="male icon" />
+        </label>
       </div>
     );
   }
