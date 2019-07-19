da888511c41aafca3e3a8209a9eb63d1fad94f69
Anatolii Pisarivskyi <pisarivskii@gmail.com>
2019-07-19 18:53:52 +0300

    feat: add flow api service

---
+++ src/services/flowService.js
@@ -0,0 +1,70 @@
+import axios from 'axios';
+
+/**
+ * Flow API service
+ */
+export default class FlowService {
+  flowId = null;
+
+  constructor(key) {
+    this.axios = axios.create();
+    this.axios.defaults.headers = {
+      Authorization: `APIKey ${key}`,
+    };
+  }
+
+  /**
+   * Save flow id
+   *
+   * @param {string} flowId - flow id
+   */
+  setFlowId(flowId) {
+    this.flowId = flowId;
+  }
+
+  /**
+   * Create flow object
+   * @async
+   * @param {any} state - flow state
+   */
+  create(state) {
+    return this.axios({
+      url: `${API_HOST}/api/v2/persons/widget/`,
+      method: 'POST',
+      data: {
+        state,
+      },
+    })
+      .then((response) => {
+        this.flowId = response.data.uuid;
+        return this.flowId;
+      });
+  }
+
+  /**
+   * Get flow object details
+   *
+   * @param {string} flowId - flow object id
+   */
+  get(flowId = this.flowId) {
+    return this.axios({
+      url: `${API_HOST}/api/v2/persons/widget/${flowId}/`,
+      method: 'GET',
+    })
+      .then(response => response.data);
+  }
+
+  /**
+   * Patch flow object details
+   *
+   * @param {string} flowId - flow object id
+   */
+  update(data, flowId = this.flowId) {
+    return this.axios({
+      url: `${API_HOST}/api/v2/persons/widget/${flowId}/`,
+      method: 'PATCH',
+      data,
+    })
+      .then(response => response.data);
+  }
+}
